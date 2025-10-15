c65d126a2891d8ba72c8bf3435c7c436e69f2e97
TR1432 <ppfaddy@gmail.com>
2025-10-15 17:27:58 +0100

    Feat: Minished Main hero section
    Chore: Left space for Image

--- src/screens/hero-screen.jsx
+++ src/screens/hero-screen.jsx
@@ -7,14 +7,28 @@ const HeroScreen = () => {
       <Navbar />
       <main className="flex flex-col space-y-5 py-[5px] px-[3px] items-center justify-center">
         <h3 className="text-center font-medium text-main text-[12px]">Make Waves. Be heard.</h3>
-        <div className="flex flex-col items-center gap-y-2.5">
+        <div className="flex flex-col items-center gap-y-2.5 leading-[76.8px]">
           <h1 className="text-center font-medium text-[58.25px]">A System,Tool & Voice</h1>
-          <div className="flex space-x-3 items-center">
+          <div className="flex space-x-3 items-center max-h-[84px]">
             <h1 className="text-center font-medium text-[58.25px]">that Listens</h1>
             <EchoAnimation/>
           </div>
         </div>
+        <div className="flex flex-col items-center text-sub-text text-center text-[16px] font-normal ">
+          <h4 className="leading-8">Let&apos;s build the future together, We don&apos;t just need a</h4>
+          <h4>Voice - We Need a System That Listens</h4>
+        </div>
+        <div className="flex justify-center items-start self-stretch space-x-2.5">
+          <div className="p-4 flex items-start rounded-full email-cta">
+            <input type="email" name="email" placeholder="name@email.com"
+             className="mr-auto md:w-[23vw]"
+            />
+          </div>
+          <button className="md:px-[27px] md:py-[15px] bg-light rounded-[20px]">Join the Waitlist!</button>
+        </div>
       </main>
+      {/* Big Image */}
+      
     </section>
   );
 };
